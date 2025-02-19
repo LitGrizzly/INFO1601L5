d869dc668b9d479fe951a9adc0b620f463e19702
LitGrizzly <jervaanhabib22@gmail.com>
2025-02-19 15:51:48 -0400

    lab 5 commit

--- index.js
+++ index.js
@@ -1,27 +1,74 @@
-//Create a constructor a functions which builds object for us
-function createPerson(name, height, weight) {
-  return { name: name, height: height, weight: weight };
+//object literal
+let bob = {
+  fname: "bob",
+  lname: "smith",
+  age: 18,
+  height: 6,
+  transcript:[
+    {
+      course: 'INFO 1603',
+      grades: [ 89, 34, 67 ]
+    },
+    {
+      course: 'INFO 1601',
+      grades: [ 89, 34, 67 ]
+    }
+  ]
+};
+
+let sally = {
+  fname: "sally",
+  lname: "smith",
+  age: 18,
+  height: 6,
+  transcript:[
+    {
+      course: 'INFO 1601',
+      grades: [ 100, 89, 79 ]
+    }
+  ]
+};
+
+let paul = {
+  fname: "paul",
+  lname: "smith",
+  age: 18,
+  height: 6,
+  transcript:[
+    {
+      course: 'INFO 1600',
+      grades: [ 89, 34, 67 ]
+    }
+  ]
+};
+
+
+const students = [bob, sally, paul];
+
+function getAverageGrade(student, course){
+
+  for(let trans of student.transcript){
+    if(trans.course===course){
+      let sum = 0;
+      for(let grade of trans.grades){
+        sum+=grade;
+      }
+      return sum / trans.grades.length;
+    }
+
+}
 }
 
-function calcBMI(weight, height) {
-  return weight / (height * height);
+function getAssignmentMark(student, course, num){
+
 }
 
-function avgBMI(people) {
-  let sum = 0;
-  for (let person of people) {
-    //sum the bmi of each person
-    sum += calcBMI(person.weight, person.height);
-  }
-  //calculate average
-  return sum / people.length;
-}
 
-//create a collection of people
-let people = [
-  createPerson("Sally", 60, 2.5),
-  createPerson("Ben", 81, 3),
-  createPerson("Shelly", 50, 1.7)
-];
 
-console.log(avgBMI(people));
+
+
+
+
+
+
+console.log(getAverageGrade(paul,'INFO 1600'));
